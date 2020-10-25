40c1ace5e87478ee020757d57ead02c1a2c89f39
shahbaj <shahbaj.49408@gmail.com>
2020-10-25 10:51:27 +0530

    Added: FlipCoin Simulation.

--- ProgrammingConstruct.sh
+++ ProgrammingConstruct.sh
@@ -38,3 +38,46 @@ printf "%s\n" "${Arithematic[@]}" | sort -nr
 echo "Ascending sorted array"
 printf "%s\n" "${Arithematic[@]}" | sort -n
 
+
+echo "WELCOME TO FLIP COIN SIMULATION PROBLEM"
+function flipcoin() {
+head=0;
+tail=0;
+while [ "$head" -lt 21 ] && [ "$tail" -lt 21 ]
+do
+
+        a=$(( RANDOM%2 ));
+        if [ $a == 1 ];
+        then
+                head=$(($head+1))
+        else
+                tail=$(($tail+1))
+        fi
+done
+
+if [ "$head" -eq 21 ] && [ "$tail" -eq 21 ]
+then
+        if [ "$head" -eq 21 ]
+        then
+                while [ "$(($head-$tail))" -gt 2 ]
+                do
+                        tail=$(($tail+1))
+                done
+        else
+                while [ "$(($tail-$head))" -gt 2 ]
+                do
+                        head=$(($head+1))
+                done
+        fi
+elif [ "$head" -eq 21 ]
+then
+        echo "Head won by" $(($head-$tail))
+else
+        echo "Tail won by" $(($tail-$head))
+fi
+echo "Final head value" $head
+echo "Final tail count" $tail
+}
+flipcoin
+
+
